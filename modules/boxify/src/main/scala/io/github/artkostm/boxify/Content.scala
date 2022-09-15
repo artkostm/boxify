f99dad0b736dfb627798abe619d0f172b41cbabe
Artsiom Chuiko <artkostm@gmail.com>
2022-09-15 22:25:33 +0400

    add content

---
+++ modules/boxify/src/main/scala/io/github/artkostm/boxify/Content.scala
@@ -0,0 +1,10 @@
+package io.github.artkostm.boxify
+
+import cats.Show
+
+enum Content:// derives Show:
+  case Blank                  // No content.
+  case Text(inner: String)    // A raw string.
+  case Row(inner: List[Box]) // A row of sub-boxes.
+  case Col(inner: List[Box]) // A column of sub-boxes.
+  case SubBox(horizontal: Alignment, vertical: Alignment, inner: Box) // A sub-box with a specified alignment.
